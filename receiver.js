f50778598b9185663c6f661b391161846b665250
Jose Luis Sánchez <jose.sanchez.olivero@accenture.com>
2019-04-26 09:20:41 +0200

    Perform message listener events.

--- receiver.js
+++ receiver.js
@@ -1,29 +1,56 @@
 (function (global) {
     
+    hideAllPlayers();
     cast.framework.CastReceiverContext.getInstance().setLoggerLevel(cast.framework.LoggerLevel.DEBUG);
     
     global.onload = function() {
+
+        //hideAllPlayers();
+
         var context = cast.framework.CastReceiverContext.getInstance();
         
         console.log('starting the receiver application');
-        var element = document.getElementById("subtitle");
         
         const CUSTOM_CHANNEL = 'urn:x-cast:es.jolusan.colorsbattlecast';
         context.addCustomMessageListener(CUSTOM_CHANNEL, customEvent => {
-            console.log('addCustomMessageListener:');
-            console.log(customEvent);
-            element.innerHTML = customEvent.data.value;
-           const objToSender = 
-              {
-                type: 'status',
-                message: 'Received: ' + customEvent.data.value
-              };
+            console.log('addCustomMessageListener:' + customEvent);
+            
+            let action = String(customEvent.data.action);
+            let key = customEvent.data.key;
+            let value = customEvent.data.value;
 
-            context.sendCustomMessage(CUSTOM_CHANNEL, undefined, objToSender);
-            if (customEvent.data.action == "color_cell"){
-              document.getElementById(customEvent.data.key).style.backgroundColor = customEvent.data.value;
+            if (action == "init_screen"){
+              document.getElementById("subtitle2").innerHTML = "Game code: " + value;
+            
+            } else if (action.startsWith("player_")){
+              document.getElementById(action + "_slot").style.display = "block";
+              document.getElementById(action + "_name").innerHTML = key;
+              document.getElementById(action + "_color").style.backgroundColor = value;
+            } else if (action == "set_time"){
+              document.getElementById("subtitle").innerHTML = "Time to play";
+              document.getElementById("subtitle2").innerHTML = value;
+          
+            } else if (action == "update_player_score"){
+              document.getElementById(key + "_score").innerHTML = value;
+          
+            } else if (action == "change_cell_color"){
+              document.getElementById(key).style.backgroundColor = value;
+            
+            } else if (action == "finish_screen"){
+              document.getElementById("subtitle").innerHTML = "Game over!: ";
+              document.getElementById("subtitle2").innerHTML = "Winner: " + value;
             }
          
+
+
+            //Send message received to device
+            const objToSender = 
+                {
+                  type: 'Action: ' + action,
+                  message: 'Key: ' + key + '. Value: ' + value
+                };
+            context.sendCustomMessage(CUSTOM_CHANNEL, undefined, objToSender);
+
         });
         
         const options = new cast.framework.CastReceiverOptions();
@@ -33,5 +60,15 @@
         context.start(options);
 
     };
+
+    function hideAllPlayers(){
+      var i;
+      for (i = 0; i < 8; i++) { 
+        let slotId = "player_" + i + "_slot";
+        document.getElementById(slotId).style.display = "none";
+        //document.getElementById(slotId).style.display = "block";//to show
+
+      }
+    }
 
 }(this));
