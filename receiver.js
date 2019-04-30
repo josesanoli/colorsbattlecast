07e701d560648c1a21d5b92526d771980216439e
Jose Luis Sánchez <jose.sanchez.olivero@accenture.com>
2019-04-30 13:00:06 +0200

    Refactor variables. Added start countdown. Check if code is created to response.

--- receiver.js
+++ receiver.js
@@ -1,7 +1,33 @@
 (function (global) {
-    
-    hideAllPlayers();
-    cast.framework.CastReceiverContext.getInstance().setLoggerLevel(cast.framework.LoggerLevel.DEBUG);
+  
+  // Messages
+  const MSG_GET_GAME_CODE = "get_game_code";
+  const MSG_INIT_SCREEN = "init_screen";
+  const MSG_START_COUNTDOWN = "start_countdown";
+  const MSG_START_GAME = "start_game";
+  const MSG_EDIT_PLAYER = "player_";
+  const MSG_SET_TIME = "set_time";
+  const MSG_PLAYER_SCORE = "player_score";
+  const MSG_CELL_COLOR = "cell_color";
+  const MSG_FINISH_SCREEN = "finish_screen";
+
+  // Cast
+  const CUSTOM_CHANNEL = 'urn:x-cast:es.jolusan.colorsbattlecast';
+
+  // Variables
+  var gameCode = "0";
+
+  // Colors
+  const DEFAULT_CELL_COLOR = "#ddddff";
+  const COUNTDOWN_CELL_COLOR = "#222222";
+  const PLAYER_COLOR = "#eeeeee";
+  const SCORE_COLOR = "#222222";
+  const BEST_PLAYER_COLOR = "#a67c00";
+  const BEST_SCORE_COLOR = "#876152";
+
+  hideAllPlayers();
+
+  cast.framework.CastReceiverContext.getInstance().setLoggerLevel(cast.framework.LoggerLevel.DEBUG);
     
     global.onload = function() {
       
@@ -9,7 +35,6 @@
         
         console.log('starting the receiver application');
         
-        const CUSTOM_CHANNEL = 'urn:x-cast:es.jolusan.colorsbattlecast';
         context.addCustomMessageListener(CUSTOM_CHANNEL, customEvent => {
             console.log('addCustomMessageListener:' + customEvent);
             
@@ -17,11 +42,25 @@
             let key = String(customEvent.data.key);
             let value = String(customEvent.data.value);
 
-            if (action == "init_screen"){
+            if (action == MSG_GET_GAME_CODE){
+              const objToSender = 
+                {
+                  type: action,
+                  message: gameCode
+                };
+              context.sendCustomMessage(CUSTOM_CHANNEL, undefined, objToSender);
+          
+            } else if (action == MSG_INIT_SCREEN){
               document.getElementById("subtitle").innerHTML = key;
               document.getElementById("subtitle2").innerHTML = value;
+              gameCode = value;
             
-            } else if (action.startsWith("player_")){
+            } else if (action == MSG_START_COUNTDOWN){
+              document.getElementById("subtitle").innerHTML = key;
+              document.getElementById("subtitle2").innerHTML = value;
+              startCountDown();
+            
+            } else if (action.startsWith(MSG_EDIT_PLAYER)){
               if (key != ""){
                 document.getElementById(action + "_slot").style.display = "flex";
                 document.getElementById(action + "_name").innerHTML = key;
@@ -31,22 +70,22 @@
                 document.getElementById(action + "_slot").style.display = "none";
               }
 
-            } else if (action == "set_time"){
-              document.getElementById("subtitle").innerHTML = "Time to play";
+            } else if (action == MSG_SET_TIME){
+              document.getElementById("subtitle").innerHTML = key;
               document.getElementById("subtitle2").innerHTML = value;
           
-            } else if (action == "update_player_score"){
+            } else if (action == MSG_PLAYER_SCORE){
               document.getElementById(key + "_score").innerHTML = value;
               checkBestScore();
 
-            } else if (action == "change_cell_color"){
+            } else if (action == MSG_CELL_COLOR){
               if (value != "alpha"){
                 document.getElementById(key).style.backgroundColor = value;
               } else {
                 document.getElementById(key).style.opacity = "0.6";
               }
             
-            } else if (action == "finish_screen"){
+            } else if (action == MSG_FINISH_SCREEN){
               document.getElementById("subtitle").innerHTML = key;
               document.getElementById("subtitle2").innerHTML = value;
                 
@@ -75,7 +114,43 @@
       for (i = 0; i < 8; i++) { 
         let slotId = "player_" + i + "_slot";
         document.getElementById(slotId).style.display = "none";
+        let playerScore = "player_" + i + "_score";
+        let playerName = "player_" + i + "_name";
+        document.getElementById(playerName).style.color = PLAYER_COLOR;
+        document.getElementById(playerName).innerHTML = "-";
+        document.getElementById(playerScore).style.color = SCORE_COLOR;
+        document.getElementById(playerScore).innerHTML = "0";
       }
+    }
+
+    function startCountDown(){
+      var dissapearOrder = ["B3","C3","C2","B2","A2","A3","A4","B4","C4","D4","D3","D2","D1","C1","B1","A1"]; 
+      var i = 0;
+      for (i = 0; i < dissapearOrder.length; i++) { 
+        var cellKey = "cell" + dissapearOrder[i]
+        document.getElementById(cellKey).style.backgroundColor = COUNTDOWN_CELL_COLOR;
+      }
+
+      var counter = 0,
+      timer = setInterval(function(){
+            if (counter === dissapearOrder.length) {
+              //When finished
+              clearInterval(timer);
+              const objToSender = 
+              {
+                type: MSG_START_COUNTDOWN,
+                message: MSG_START_GAME
+              };
+              var context = cast.framework.CastReceiverContext.getInstance();
+              context.sendCustomMessage(CUSTOM_CHANNEL, undefined, objToSender);
+              
+            } else {
+              var cellKey = "cell" + dissapearOrder[counter]
+              document.getElementById(cellKey).style.backgroundColor = DEFAULT_CELL_COLOR;
+              counter++
+            }
+      },400);
+
     }
 
     function checkBestScore(){
@@ -92,13 +167,14 @@
       for (i = 0; i < 8; i++) {
         let playerScore = "player_" + i + "_score";
         let playerName = "player_" + i + "_name";
-        if (arr[i] == maxScore){
-          document.getElementById(playerName).style.color = "#a67c00";
-          document.getElementById(playerScore).style.color = "#876152";
+        if (arr[i] == maxScore && arr[i]>0){
+          document.getElementById(playerName).style.color = BEST_PLAYER_COLOR;
+          document.getElementById(playerScore).style.color = BEST_SCORE_COLOR;
         } else {
-          document.getElementById(playerName).style.color = "#eeeeee";
-          document.getElementById(playerScore).style.color = "#222222";
+          document.getElementById(playerName).style.color = PLAYER_COLOR;
+          document.getElementById(playerScore).style.color = SCORE_COLOR;
         }
+        
       }
     }
 
