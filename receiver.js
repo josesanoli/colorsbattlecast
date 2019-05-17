999a9e61eacf9bf7e0b83cfb244bea0d7d0d2984
Jose Luis Sánchez <jose.sanchez.olivero@accenture.com>
2019-05-17 15:01:47 +0200

    Buffered in string

--- receiver.js
+++ receiver.js
@@ -300,7 +300,7 @@
     const loadRequestData = new cast.framework.messages.LoadRequestData();
     loadRequestData.media = new cast.framework.messages.MediaInformation();
     loadRequestData.media.contentId = soundSrc;
-    loadRequestData.media.streamType = media.BUFFERED; 
+    loadRequestData.media.streamType = "BUFFERED"; 
     loadRequestData.autoplay = true;
 
     playerManager.load(loadRequestData).then(
